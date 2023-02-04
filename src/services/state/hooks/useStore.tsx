a21f54758707a272a1f22e6b50407d0e2aad6ae2
SanjeevE1996 <sanjeevdhillon12@hotmail.co.uk>
2023-02-04 20:57:35 +0000

    chore:small naming update for MST file

--- src/services/state/hooks/useStore.tsx
+++ src/services/state/hooks/useStore.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import { RootStore } from "../rootStore";
+import { RootStore } from "..";
 
 // create the root store
 const rootStore = RootStore.create();
