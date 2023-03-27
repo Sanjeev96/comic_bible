eee404b9a8748c90778caaaff47703559940c27d
Sanjeev96 <sanjeevdhillon12@hotmail.co.uk>
2023-03-27 23:20:27 +0100

    WIP thunk logic

---
+++ src/services/state/dataSlice.ts
@@ -0,0 +1,15 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// export interface DataState {
+//     recentComics: Recent
+// }
+
+// const initalState: DataState = {
+//     recentComics: []
+//     searchedComics: []
+    
+// }
+
+// export const DataSlice = createSlice({
+
+// })
